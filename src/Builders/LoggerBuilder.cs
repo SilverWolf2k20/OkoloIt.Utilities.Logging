a0477d6d933f8be6d194ef6de303601aa3157952
egorw <egorw@DESKTOP-TCDMSCK>
2023-02-23 16:56:59 +0300

    feat: добавлен строитель логера

---
+++ src/Builders/LoggerBuilder.cs
@@ -0,0 +1,93 @@
+﻿using OkoloIt.Utilities.Logging.Configuration;
+
+namespace OkoloIt.Utilities.Logging
+{
+    /// <summary>
+    /// Строитель логера.
+    /// </summary>
+    public class LoggerBuilder : ILoggerBuilder
+    {
+        #region Private Fields
+
+        private readonly LoggerConfigurations _configuration = new();
+
+        #endregion Private Fields
+
+        #region Public Methods
+
+        /// <summary>
+        /// Строит логер, наследуемый от <see cref="ILogger"/>.
+        /// </summary>
+        /// <returns>Созданный логер.</returns>
+        public ILogger Build()
+        {
+            if (_configuration.UseAsyncWrite)
+                return new AsyncLogger(_configuration);
+
+            return new Logger(_configuration);
+        }
+
+        /// <summary>
+        /// Устанавливает минимальный уровень логирования.
+        /// </summary>
+        /// <param name="level">Минимальный уровень логирования.</param>
+        /// <returns>
+        /// Экземпляр строителя, наследуемого от <see cref="ILoggerBuilder"/>.
+        /// </returns>
+        public ILoggerBuilder SetMinimalLevel(LogLevel level)
+        {
+            return this;
+        }
+
+
+        /// <summary>
+        /// Устанавливает формат вывода сообщений.
+        /// </summary>
+        /// <returns>
+        /// Экземпляр строителя, наследуемого от <see cref="ILoggerBuilder"/>.
+        /// </returns>
+        public ILoggerBuilder SetWriteFormat()
+        {
+            return this;
+        }
+
+        /// <summary>
+        /// Настраивает логер на асинхронную работу.
+        /// </summary>
+        /// <returns>
+        /// Экземпляр строителя, наследуемого от <see cref="ILoggerBuilder"/>.
+        /// </returns>
+        public ILoggerBuilder UseAsync()
+        {
+            _configuration.UseAsyncWrite = true;
+            return this;
+        }
+
+        /// <summary>
+        /// Устанавливает вывод сообщений в консоль.
+        /// </summary>
+        /// <returns>
+        /// Экземпляр строителя, наследуемого от <see cref="ILoggerBuilder"/>.
+        /// </returns>
+        public ILoggerBuilder WriteToConsole()
+        {
+            _configuration.InConsole = true;
+            return this;
+        }
+
+        /// <summary>
+        /// Устанавливает вывод сообщений в файл.
+        /// </summary>
+        /// <returns>
+        /// Экземпляр строителя, наследуемого от <see cref="ILoggerBuilder"/>.
+        /// </returns>
+        public ILoggerBuilder WriteToFile(string fileName)
+        {
+            _configuration.FileName  = fileName;
+            _configuration.InConsole = false;
+            return this;
+        }
+
+        #endregion Public Methods
+    }
+}
