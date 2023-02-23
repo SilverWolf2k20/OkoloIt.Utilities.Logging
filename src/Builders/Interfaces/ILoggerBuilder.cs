002cd6c1092ad2b7df49cfbc5632fc427e52e149
Ihor <egorwolf2k18@mail.ru>
2023-02-23 16:22:20 +0300

    feat: добавлены интерфейсы логера
    Реализован интерфейс строителя и логера.

---
+++ src/Builders/Interfaces/ILoggerBuilder.cs
@@ -0,0 +1,61 @@
+﻿using OkoloIt.Utilities.Logging.Configuration;
+
+namespace OkoloIt.Utilities.Logging
+{
+    /// <summary>
+    /// Интерфейс строителя логера.
+    /// </summary>
+    public interface ILoggerBuilder
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Строит логер, наследуемый от <see cref="ILogger"/>.
+        /// </summary>
+        /// <returns>Созданный логер.</returns>
+        public ILogger Build();
+
+        /// <summary>
+        /// Устанавливает минимальный уровень логирования.
+        /// </summary>
+        /// <param name="level">Минимальный уровень логирования.</param>
+        /// <returns>
+        /// Экземпляр строителя, наследуемого от <see cref="ILoggerBuilder"/>.
+        /// </returns>
+        public ILoggerBuilder SetMinimalLevel(LogLevel level);
+
+        /// <summary>
+        /// Устанавливает формат вывода сообщений.
+        /// </summary>
+        /// <returns>
+        /// Экземпляр строителя, наследуемого от <see cref="ILoggerBuilder"/>.
+        /// </returns>
+        public ILoggerBuilder SetWriteFormat();
+
+        /// <summary>
+        /// Настраивает логер на асинхронную работу.
+        /// </summary>
+        /// <returns>
+        /// Экземпляр строителя, наследуемого от <see cref="ILoggerBuilder"/>.
+        /// </returns>
+        public ILoggerBuilder UseAsync();
+
+        /// <summary>
+        /// Устанавливает вывод сообщений в консоль.
+        /// </summary>
+        /// <returns>
+        /// Экземпляр строителя, наследуемого от <see cref="ILoggerBuilder"/>.
+        /// </returns>
+        public ILoggerBuilder WriteToConsole();
+
+        /// <summary>
+        /// Устанавливает вывод сообщений в файл.
+        /// </summary>
+        /// <returns>
+        /// Экземпляр строителя, наследуемого от <see cref="ILoggerBuilder"/>.
+        /// </returns>
+        public ILoggerBuilder WriteToFile(string fileName);
+
+        #endregion Public Methods
+    }
+}
---
+++ src/Interfaces/ILogger.cs
@@ -0,0 +1,48 @@
+﻿namespace OkoloIt.Utilities.Logging
+{
+    /// <summary>
+    /// Интерфейс логера.
+    /// </summary>
+    public interface ILogger
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Выводит сообщение отладки.
+        /// </summary>
+        /// <param name="message">Текст сообщения.</param>
+        public void Debug(string message);
+
+        /// <summary>
+        /// Выводит сообщение ошибки.
+        /// </summary>
+        /// <param name="message">Текст сообщения.</param>
+        public void Error(string message);
+
+        /// <summary>
+        /// Выводит сообщение критической ошибки.
+        /// </summary>
+        /// <param name="message">Текст сообщения.</param>
+        public void Fatal(string message);
+
+        /// <summary>
+        /// Выводит информативное сообщение.
+        /// </summary>
+        /// <param name="message">Текст сообщения.</param>
+        public void Info(string message);
+
+        /// <summary>
+        /// Выводит сообщение.
+        /// </summary>
+        /// <param name="message">Текст сообщения.</param>
+        public void Trace(string message);
+
+        /// <summary>
+        /// Выводит сообщение о не штатном поведении.
+        /// </summary>
+        /// <param name="message">Текст сообщения.</param>
+        public void Warn(string message);
+
+        #endregion Public Methods
+    }
+}
