0298ee1b1a54ed4207a183f357f485f41623ec19
Ihor <egorwolf2k18@mail.ru>
2023-02-23 16:56:32 +0300

    feat: добавлены классы конфигурации
    Добавлено перечисление уровня логов и конфигурация логера.

---
+++ src/Configuration/LoggerConfigurations.cs
@@ -0,0 +1,28 @@
+﻿namespace OkoloIt.Utilities.Logging.Configuration
+{
+    /// <summary>
+    /// Конфигурация логера.
+    /// </summary>
+    public class LoggerConfigurations
+    {
+        /// <summary>
+        /// Запись в консоль.
+        /// </summary>
+        public bool InConsole { get; set; } = default;
+
+        /// <summary>
+        /// Использовать асинхронную запись.
+        /// </summary>
+        public bool UseAsyncWrite { get; set; } = false;
+
+        /// <summary>
+        /// Имя файла для записи.
+        /// </summary>
+        public string FileName { get; set; } = "logs.log";
+
+        /// <summary>
+        /// Минимальный уровень логов.
+        /// </summary>
+        public LogLevel MinimalLevel { get; set; } = LogLevel.Trace;
+    }
+}
