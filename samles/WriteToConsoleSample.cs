c695927a8e227e41618716695ec6999d7bffac16
egorw <egorw@DESKTOP-TCDMSCK>
2023-02-23 18:24:18 +0300

    feat: добавлены примеры работы с библиотекой

---
+++ samles/WriteToConsoleSample.cs
@@ -0,0 +1,19 @@
+﻿using OkoloIt.Utilities.Logging.Configuration;
+
+namespace OkoloIt.Utilities.Logging.Samples
+{
+    internal class WriteToConsoleSample : WriterSample
+    {
+        protected internal override void InitializationLogger()
+        {
+            ILogger logger = new LoggerBuilder()
+                .SetMinimalLevel(LogLevel.Trace)
+                .SetWriteFormat()
+                .WriteToConsole()
+                .UseAsync()
+                .Build();
+
+            logger.Info("Создан логер!");
+        }
+    }
+}
---
+++ samles/WriteToFileSample.cs
@@ -0,0 +1,19 @@
+﻿using OkoloIt.Utilities.Logging.Configuration;
+
+namespace OkoloIt.Utilities.Logging.Samples
+{
+    internal class WriteToFileSample : WriterSample
+    {
+        protected internal override void InitializationLogger()
+        {
+            ILogger logger = new LoggerBuilder()
+                .SetMinimalLevel(LogLevel.Trace)
+                .SetWriteFormat()
+                .WriteToFile("programm_logs.log")
+                .UseAsync()
+                .Build();
+
+            logger.Info("Создан логер!");
+        }
+    }
+}
