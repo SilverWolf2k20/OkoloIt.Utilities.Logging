075c4f416b7eb904b9bd95687f7b040b0b11278d
egorw <egorw@DESKTOP-TCDMSCK>
2023-02-23 16:56:32 +0300

    feat: добавлены классы конфигурации
    Добавлено перечисление уровня логов и конфигурация логера.

---
+++ src/Configuration/LogLevel.cs
@@ -0,0 +1,43 @@
+﻿namespace OkoloIt.Utilities.Logging.Configuration
+{
+    /// <summary>
+    /// Уровни логов.
+    /// </summary>
+    public enum LogLevel : byte
+    {
+        /// <summary>
+        /// Трасировка.
+        /// </summary>
+        Trace,
+
+        /// <summary>
+        /// Отладка.
+        /// </summary>
+        Debug,
+
+        /// <summary>
+        /// Информация.
+        /// </summary>
+        Info,
+
+        /// <summary>
+        /// Предумпреждение.
+        /// </summary>
+        Warn,
+
+        /// <summary>
+        /// Ошибка.
+        /// </summary>
+        Error,
+
+        /// <summary>
+        /// Критическая ошибка.
+        /// </summary>
+        Fatal,
+
+        /// <summary>
+        /// Отключено.
+        /// </summary>
+        Off,
+    }
+}
