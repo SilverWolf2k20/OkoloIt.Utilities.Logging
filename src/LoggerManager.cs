5bc3bd32c1072e6ed102f969365f223c5c12cd29
Ihor <egorwolf2k18@mail.ru>
2023-02-23 17:10:05 +0300

    feat: добавлен менеджер логеров

---
+++ src/LoggerManager.cs
@@ -0,0 +1,42 @@
+﻿namespace OkoloIt.Utilities.Logging
+{
+    /// <summary>
+    /// Менеджер логера.
+    /// </summary>
+    public sealed class LoggerManager
+    {
+        #region Private Fields
+
+        private static ILogger? _logger;
+
+        #endregion Private Fields
+
+        #region Public Methods
+
+        /// <summary>
+        /// Возвращает логер, если он существует.
+        /// </summary>
+        /// <returns>Логер.</returns>
+        /// <exception cref="NullReferenceException"> если логер не был создан.</exception>
+        public static ILogger GetLogger()
+        {
+            if (_logger is null)
+                throw new NullReferenceException("Не существует класса ILogger!");
+
+            return _logger;
+        }
+
+        #endregion Public Methods
+
+        #region Internal Methods
+
+        /// <summary>
+        /// Устанавливает логер.
+        /// </summary>
+        /// <param name="logger">Логер.</param>
+        internal static void SetLogger(ILogger logger)
+            => _logger = logger;
+
+        #endregion Internal Methods
+    }
+}
