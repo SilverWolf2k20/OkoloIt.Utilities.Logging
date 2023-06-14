1c945a0c52715f71d91e942d7ce3db3f7ce9b24e
Ihor <egorwolf2k18@mail.ru>
2023-06-14 20:12:51 +0300

    feat: убрано возвращение null для логера

--- src/LoggerManager.cs
+++ src/LoggerManager.cs
@@ -1,16 +1,27 @@
-﻿namespace OkoloIt.Utilities.Logging;
+﻿using OkoloIt.Utilities.Logging.Configuration;
+
+namespace OkoloIt.Utilities.Logging;
 
 /// <summary>
 /// Менеджер логера.
 /// </summary>
 public sealed class LoggerManager
 {
+    #region Private Fields
+
+    private static ILogger? _logger;
+
+    #endregion Private Fields
+
     #region Public Properties
 
     /// <summary>
     /// Текущий логгер.
     /// </summary>
-    public static ILogger? CurrentLogger { get; internal set; }
+    public static ILogger CurrentLogger {
+        get => _logger ?? new Logger(new LoggerConfiguration(), null);
+        set => _logger = value;
+    }
 
     #endregion Public Properties
 }
