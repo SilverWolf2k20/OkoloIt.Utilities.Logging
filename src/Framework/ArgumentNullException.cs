f09e646f3e23ad58f3c2f0604b508f5a9111b82b
Ihor <egorwolf2k18@mail.ru>
2024-06-05 19:48:15 +0300

    fix: исправлены ошибки совместимости

---
+++ src/Framework/ArgumentNullException.cs
@@ -0,0 +1,15 @@
+﻿#if NET462_OR_GREATER
+
+namespace OkoloIt.Utilities.Logging;
+
+#pragma warning disable CS1591 // Отсутствует комментарий XML для открытого видимого типа или члена
+public static class ArgumentNullException
+{
+    public static void ThrowIfNull(object? obj)
+    {
+        if (obj is null)
+            throw new System.ArgumentNullException(nameof(obj));
+    }
+}
+#pragma warning restore CS1591 // Отсутствует комментарий XML для открытого видимого типа или члена
+#endif
